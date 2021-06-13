77a2ef9db1abda6d8a19a0f019d35ad14ada2498
Shubham Pandey <shubhampcpandey13@gmail.com>
2021-06-13 17:17:04 +0530

    Add calls table

--- src/app/pages/connectors/connectors-calls/connectors-calls.component.ts
+++ src/app/pages/connectors/connectors-calls/connectors-calls.component.ts
@@ -1,12 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { ViewResultButtonComponent } from 'src/app/@theme/components/smart-table/smart-table';
 
 @Component({
-  selector: 'intelowl-connectors-calls',
   templateUrl: './connectors-calls.component.html',
-  styleUrls: ['./connectors-calls.component.scss'],
 })
-export class ConnectorsCallsComponent implements OnInit {
-  constructor() {}
+export class ConnectorsCallsComponent {
+  // ng2-smart-table data source
+  source: LocalDataSource = new LocalDataSource();
 
-  ngOnInit(): void {}
+  // ng2-smart-table settings
+  settings = {
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+    },
+    pager: {
+      display: true,
+      perPage: 10,
+    },
+    columns: {
+      result: {
+        title: 'Result',
+        type: 'custom',
+        width: '3%',
+        filter: false,
+        sort: false,
+        renderComponent: ViewResultButtonComponent,
+      },
+      id: {
+        title: 'Id',
+        filter: false,
+      },
+      name: {
+        title: 'Name',
+        filter: false,
+      },
+      connectorsCalled: {
+        title: 'Connectors Called',
+        filter: false,
+      },
+      processTime: {
+        title: 'Process Time (s)',
+        filter: false,
+      },
+    },
+  };
+
+  constructor() {
+    // load data into table
+    this.source.load([
+      {
+        id: 1,
+        name: 'xD',
+        connectorsCalled: '3/3',
+        processTime: '60',
+      },
+    ]);
+  }
 }
