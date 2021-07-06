d2f82051fbca7f7146653c9ea20f74ac87d1bc41
Shubham Pandey <shubhampcpandey13@gmail.com>
2021-07-06 21:49:31 +0530

    Fix: replace analysis reports with analyzer reports (#118)

--- src/app/@core/models/models.ts
+++ src/app/@core/models/models.ts
@@ -39,7 +39,7 @@ export interface Job {
   analyzers_requested: string[] | string;
   analyzers_to_execute: string[];
   connectors_to_execute: string[];
-  analysis_reports?: any[];
+  analyzer_reports?: any[];
   connector_reports?: any[];
   received_request_time: string | Date;
   finished_analysis_time?: string | Date;
